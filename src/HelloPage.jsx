30af23b58305288aae64f3d597ae20d64c06aaaa
rodrigofrancoHHS <133037096+rodrigofrancoHHS@users.noreply.github.com>
2023-06-14 13:00:56 +0100

    HHS

--- src/HelloPage.jsx
+++ src/HelloPage.jsx
@@ -7,38 +7,36 @@ import 'ag-grid-community/styles/ag-grid.css';
 import 'ag-grid-community/styles/ag-theme-alpine.css';
 
 function HelloPage() {
-  const storedItems = localStorage.getItem('updatedItems');
-  const initialItems = storedItems
-    ? JSON.parse(storedItems)
-    : [
-      { name: 'Coca-cola', price: 1.20, quantity: 10, sold: 0},
-      { name: 'Sprite', price: 0.80, quantity: 5, sold: 0},
-      { name: 'Ice-Tea', price: 1.20, quantity: 15, sold: 0},
-      { name: 'Pepsi', price: 0.85, quantity: 14, sold: 0},
-      { name: 'Bongo', price: 0.99, quantity: 9, sold: 0},
-      { name: 'Monster', price: 1.40, quantity: 20, sold: 0},
-      { name: 'Guaraná', price: 1.00, quantity: 10, sold: 0},
-      { name: 'Sumol', price: 1.10, quantity: 1, sold: 0},
-      { name: 'Chá', price: 1.25, quantity: 4, sold: 0},
-      { name: 'Água', price: 1.30, quantity: 16, sold: 0},
-      { name: '7UP', price: 0.85, quantity: 17, sold: 0},
-      { name: 'Café', price: 0.80, quantity: 20, sold: 0}
-      ];
-  const [items, setItems] = useState(initialItems);
+  const [items, setItems] = useState([]);
+  
+  const apiUrl = 'https://localhost:7136';
+  
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/api/TodosProdutos/ListadeProdutos`);
+        const produtosCarregados = await response.json();
+  
+        setItems(produtosCarregados);
+      } catch (error) {
+        console.error('Erro ao obter os produtos da API:', error);
+      }
+    };
+  
+    fetchProducts();
+  }, [items]);
 
-  const renameItem = (index) => { // cria uma constante que irá rever todos os produtos um por um
+
+  const renameItem = async (index) => {
     const newPriceInput = document.getElementById(`newPriceInput_${index}`);
     const newPrice = newPriceInput.value;
     const newQuantityInput = document.getElementById(`newQuantityInput_${index}`);
     const newQuantity = newQuantityInput.value;
-
-    
-
-
-    if (newPrice !== '' && newQuantity !== '') { //faz a verificação se os números do novo preço e nova quantidade estão vazios
-      const updatedItems = items.map((item, i) => { // vai buscar todos os valores dos items por index, ou seja um por um
-        if (i === index) { // verifica  se o indice presente é igual ao selecionado nos inputs acima
-          return { // retorna o item e as suas novas props
+  
+    if (newPrice !== '' && newQuantity !== '') {
+      const updatedItems = items.map((item, i) => {
+        if (i === index) {
+          return {
             ...item,
             price: parseFloat(newPrice),
             quantity: parseInt(newQuantity),
@@ -46,9 +44,32 @@ function HelloPage() {
         }
         return item;
       });
+  
+      setItems(updatedItems);
 
-      setItems(updatedItems); // atualiza o estado do produto
-      localStorage.setItem('updatedItems', JSON.stringify(updatedItems)); // adiciona á localstorage
+  
+      try {
+        const response = await fetch(`${apiUrl}/api/TodosProdutos/InserirAtualizarProdutos`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(updatedItems),
+        });
+  
+        if (response.ok) {
+          const responseData = await response.json();
+          setItems(responseData);
+
+          //Limpar as caixas de texto
+          newPriceInput.value = '';
+          newQuantityInput.value = '';
+        } else {
+          console.error('Erro ao atualizar os produtos na API');
+        }
+      } catch (error) {
+        console.error('Erro ao conectar-se com a API:', error);
+      }
     }
   };
 
@@ -60,24 +81,167 @@ function HelloPage() {
   ];
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+  const [newProduct, setNewProduct] = useState({
+    name: '',
+    price: '',
+    quantity: ''
+  });
+
+  const handleNewProductChange = (event) => {
+    const { name, value } = event.target;
+    setNewProduct((prevProduct) => ({
+      ...prevProduct,
+      [name]: value
+    }));
+  };
+
+  const addNewProduct = async () => {
+    try {
+      const response = await fetch(`${apiUrl}/api/TodosProdutos/InserirAtualizarProdutos`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify([...items, newProduct])
+      });
+  
+      if (response.ok) {
+        const responseData = await response.json();
+        setItems(responseData);
+        setNewProduct({
+          name: '',
+          price: '',
+          quantity: ''
+        });
+      } else {
+        console.error('Erro ao adicionar o produto à API');
+      }
+    } catch (error) {
+      console.error('Erro ao conectar-se com a API:', error);
+    }
+  };
+  
+
+
+
+
+
+  
+
+
   return (
     <div>
-      <Link to={'/'} className='config'>
-        <button className='config'>Voltar ao Início</button>
+
+      <br/><br/>
+      
+      <Link to={'/'}>
+      <div className='flex justify-center'>
+        <h2 className="text-4xl font-bold mb-8">Máquina de Venda</h2>
+      </div>
       </Link>
 
-      <br/><br/><br/><br/><br/><br/>
+      <br/><br/>
 
-      {items.map((item, index) => (
-        <div className="item-container" key={index}>
-          <label className="item-name">Item: {item.name}</label>
-          <label className="item-name">Preço: {item.price}</label>
-          <input className="new-price-input" type="text" id={`newPriceInput_${index}`} placeholder="Insira o Novo Preço" />
-          <input className="new-quantity-input" type="text" id={`newQuantityInput_${index}`} placeholder="Insira a Nova Quantidade" />
-          <label className="item-quantity">Quantidade: {item.quantity}</label>
-          <button className="rename-button" onClick={() => renameItem(index)}>Submeter</button>
+      <div className="flex flex-wrap -mx-4">
+  {items.map((item, index) => (
+    <div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-4 mb-4" key={index}>
+      <div className="bg-blue-600 p-4 rounded-md shadow-lg">
+        <div className="flex justify-center">
+          <label className="text-2xl text-white font-serif">{item.name}</label>
         </div>
-      ))}
+
+        <br/>
+        <div className='flex justify-center'>
+          <label className="text-lg text-white font-semibold">Preço: {item.price} €</label><br/>
+        </div>
+        <input className="w-full px-4 py-2 mt-2 bg-gray-200 rounded" type="text" id={`newPriceInput_${index}`} placeholder="Insira o Novo Preço"/>
+        <br/><br/>
+        <div className='flex justify-center'>
+          <label className="text-lg text-white font-semibold">Quantidade: {item.quantity}</label><br/>
+        </div>
+        <input className="w-full px-4 py-2 mt-2 bg-gray-200 rounded" type="text" id={`newQuantityInput_${index}`} placeholder="Insira a Nova Quantidade"/>
+        <br/><br/>
+        <div className='flex justify-center'>
+          <button
+            className="px-4 py-2 mt-2 bg-black hover:bg-black text-white font-semibold rounded"
+            onClick={() => renameItem(index)}
+          >
+            Submeter
+          </button>
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/4 px-4 mb-4">
+  <div className="bg-blue-600 p-4 rounded-md shadow-lg">
+    <div className="flex justify-center">
+      <label className="text-2xl text-white font-serif">Novo Produto</label>
+    </div>
+    <br />
+    <input
+      className="w-full px-4 py-2 mt-2 bg-gray-200 rounded"
+      type="text"
+      name="name"
+      value={newProduct.name}
+      onChange={handleNewProductChange}
+      placeholder="Nome do Produto"
+    />
+    <br /><br />
+    <input
+      className="w-full px-4 py-2 mt-2 bg-gray-200 rounded"
+      type="text"
+      name="price"
+      value={newProduct.price}
+      onChange={handleNewProductChange}
+      placeholder="Preço do Produto"
+    />
+    <br /><br />
+    <input
+      className="w-full px-4 py-2 mt-2 bg-gray-200 rounded"
+      type="text"
+      name="quantity"
+      value={newProduct.quantity}
+      onChange={handleNewProductChange}
+      placeholder="Quantidade do Produto"
+    />
+    <br /><br />
+    <div className="flex justify-center">
+      <button
+        className="px-4 py-2 mt-2 bg-black hover:bg-black text-white font-semibold rounded"
+        onClick={addNewProduct}
+      >
+        Adicionar
+      </button>
+    </div>
+  </div>
+</div>
+
 
       <br/><br/><br/><br/><br/><br/>
 
